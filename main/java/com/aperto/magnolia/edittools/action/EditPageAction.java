c2886315c5a63136257d7ad8e81c3660d1175e4b
frank sommer <frank.sommer@aperto.de>
2016-01-22 12:41:13 +0100

    MGKT-403: prevent page properties on inherited components

--- main/java/com/aperto/magnolia/edittools/action/EditPageAction.java
+++ main/java/com/aperto/magnolia/edittools/action/EditPageAction.java
@@ -1,7 +1,5 @@
 package com.aperto.magnolia.edittools.action;
 
-import com.aperto.magkit.utils.NodeUtils;
-
 import info.magnolia.config.registry.DefinitionProvider;
 import info.magnolia.event.EventBus;
 import info.magnolia.jcr.util.NodeTypes;
@@ -24,6 +22,9 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+
+import static com.aperto.magkit.utils.NodeUtils.isNodeType;
+import static info.magnolia.jcr.util.NodeUtil.getNearestAncestorOfType;
 
 /**
  * Extended action to execute page properties dialog even if current item is a sub item like area or component.
@@ -52,35 +53,42 @@ public class EditPageAction extends EditElementAction {
 
     @Override
     public void execute() throws ActionExecutionException {
-
         Node pageNode = SessionUtil.getNode(_element.getWorkspace(), _element.getPath());
-        if (pageNode == null) {
-            pageNode = SessionUtil.getNode(_element.getWorkspace(), "/");
-        }
 
-        try {
-            while (pageNode != null && pageNode.getDepth() > 1 && !NodeUtils.isNodeType(pageNode, NodeTypes.Page.NAME)) {
-                pageNode = pageNode.getParent();
+        if (pageNode != null) {
+            if (isNodeType(pageNode, NodeTypes.Page.NAME)) {
+                // we are on a page node, so we could use the origin implementation
+                super.execute();
+            } else {
+                // we have to find the corresponding page node
+                openDialogForCorrespondingPage(pageNode);
             }
-            if (NodeUtils.isNodeType(pageNode, NodeTypes.Page.NAME)) {
-                final JcrNodeAdapter item = new JcrNodeAdapter(pageNode);
-                String templateId = NodeTypes.Renderable.getTemplate(pageNode);
-                DefinitionProvider<TemplateDefinition> templateDefinition = _templateDefinitionRegistry.getProvider(templateId);
-                String dialogId = templateDefinition.get().getDialog();
-                final FormDialogPresenter formDialogPresenter = _dialogPresenterFactory.createFormDialogPresenter(dialogId);
-                formDialogPresenter.start(item, dialogId, _subAppContext, new EditorCallback() {
+        }
+    }
 
-                    @Override
-                    public void onSuccess(String actionName) {
-                        _eventBus.fireEvent(new ContentChangedEvent(item.getItemId()));
-                        formDialogPresenter.closeDialog();
-                    }
+    private void openDialogForCorrespondingPage(Node currentNode) throws ActionExecutionException {
+        try {
+            if (currentNode.getDepth() > 1) {
+                Node pageNode = getNearestAncestorOfType(currentNode, NodeTypes.Page.NAME);
+                if (isNodeType(pageNode, NodeTypes.Page.NAME)) {
+                    final JcrNodeAdapter item = new JcrNodeAdapter(pageNode);
+                    String templateId = NodeTypes.Renderable.getTemplate(pageNode);
+                    DefinitionProvider<TemplateDefinition> templateDefinition = _templateDefinitionRegistry.getProvider(templateId);
+                    String dialogId = templateDefinition.get().getDialog();
+                    final FormDialogPresenter formDialogPresenter = _dialogPresenterFactory.createFormDialogPresenter(dialogId);
+                    formDialogPresenter.start(item, dialogId, _subAppContext, new EditorCallback() {
+                        @Override
+                        public void onSuccess(String actionName) {
+                            _eventBus.fireEvent(new ContentChangedEvent(item.getItemId()));
+                            formDialogPresenter.closeDialog();
+                        }
 
-                    @Override
-                    public void onCancel() {
-                        formDialogPresenter.closeDialog();
-                    }
-                });
+                        @Override
+                        public void onCancel() {
+                            formDialogPresenter.closeDialog();
+                        }
+                    });
+                }
             }
         } catch (RepositoryException e) {
             throw new ActionExecutionException(e);
--- main/java/com/aperto/magnolia/edittools/rule/IsElementEditableRule.java
+++ main/java/com/aperto/magnolia/edittools/rule/IsElementEditableRule.java
@@ -1,7 +1,6 @@
 package com.aperto.magnolia.edittools.rule;
 
 import com.aperto.magkit.utils.NodeUtils;
-
 import info.magnolia.config.registry.DefinitionProvider;
 import info.magnolia.jcr.util.NodeTypes;
 import info.magnolia.jcr.util.SessionUtil;
@@ -10,6 +9,7 @@ import info.magnolia.pages.app.editor.availability.AbstractElementAvailabilityRu
 import info.magnolia.rendering.template.TemplateDefinition;
 import info.magnolia.rendering.template.registry.TemplateDefinitionRegistry;
 import info.magnolia.ui.vaadin.gwt.client.shared.AbstractElement;
+import info.magnolia.ui.vaadin.gwt.client.shared.ComponentElement;
 import info.magnolia.ui.vaadin.gwt.client.shared.PageElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +18,8 @@ import javax.inject.Inject;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
+import static info.magnolia.jcr.util.NodeUtil.getNearestAncestorOfType;
+import static info.magnolia.jcr.util.NodeUtil.isNodeType;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 /**
@@ -28,7 +30,6 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
  * @since 30.06.2015
  */
 public class IsElementEditableRule extends AbstractElementAvailabilityRule<AbstractElement> {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(IsElementEditableRule.class);
 
     private TemplateDefinitionRegistry _templateDefinitionRegistry;
@@ -42,12 +43,11 @@ public class IsElementEditableRule extends AbstractElementAvailabilityRule<Abstr
         boolean result = false;
         if (element instanceof PageElement) {
             result = element.getDialog() != null;
-        } else if (element != null && isNotBlank(element.getPath()) && isNotBlank(element.getWorkspace())) {
+        } else if (isValidElement(element)) {
             try {
                 Node pageNode = SessionUtil.getNode(element.getWorkspace(), element.getPath());
-                //Node pageNode = SessionUtil.getNode(element.getWorkspace(), element.getPath());
-                while (pageNode != null && pageNode.getDepth() > 1 && !NodeUtils.isNodeType(pageNode, NodeTypes.Page.NAME)) {
-                    pageNode = pageNode.getParent();
+                if (!isNodeType(pageNode, NodeTypes.Page.NAME)) {
+                    pageNode = getNearestAncestorOfType(pageNode, NodeTypes.Page.NAME);
                 }
                 if (NodeUtils.isNodeType(pageNode, NodeTypes.Page.NAME)) {
                     DefinitionProvider<TemplateDefinition> templateDefinition = _templateDefinitionRegistry.getProvider(NodeUtils.getTemplate(pageNode));
@@ -59,6 +59,16 @@ public class IsElementEditableRule extends AbstractElementAvailabilityRule<Abstr
         }
 
         return result;
+    }
+
+    /**
+     * Checks if the current element is not inherited. There is obviously no simple check for that.
+     *
+     * @param element current element (component or area)
+     * @return is allowed element
+     */
+    private boolean isValidElement(final AbstractElement element) {
+        return element != null && isNotBlank(element.getPath()) && isNotBlank(element.getWorkspace()) && (!(element instanceof ComponentElement) || ((ComponentElement) element).getEditable());
     }
 
     @Inject
