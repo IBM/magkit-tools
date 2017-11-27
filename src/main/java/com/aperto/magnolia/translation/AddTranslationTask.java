b67241d28411a7eec31b83b626bd9a0ded482719
frank sommer <frank.sommer@aperto.com>
2017-11-27 13:02:42 +0100

    MGKT-434: refactor add translation task

--- src/main/java/com/aperto/magnolia/translation/AddTranslationTask.java
+++ src/main/java/com/aperto/magnolia/translation/AddTranslationTask.java
@@ -2,7 +2,6 @@ package com.aperto.magnolia.translation;
 
 import com.aperto.magkit.nodebuilder.task.NodeBuilderTask;
 import com.aperto.magnolia.translation.TranslationNodeTypes.Translation;
-import info.magnolia.jcr.util.NodeTypes;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.AbstractTask;
 import info.magnolia.module.delta.ArrayDelegateTask;
@@ -18,7 +17,6 @@ import static com.aperto.magkit.nodebuilder.NodeOperationFactory.addOrSetPropert
 import static com.aperto.magnolia.translation.TranslationNodeTypes.WS_TRANSLATION;
 import static info.magnolia.cms.core.Path.getValidatedLabel;
 import static info.magnolia.jcr.nodebuilder.task.ErrorHandling.logging;
-import static java.util.Locale.ENGLISH;
 
 /**
  * Add all not existing keys from a properties file to translation workspace.
@@ -26,31 +24,35 @@ import static java.util.Locale.ENGLISH;
  * @author diana.racho (Aperto AG)
  */
 public class AddTranslationTask extends AbstractTask {
-    private static final String ND_TRANSLATION = "translation";
+    private final String _baseName;
+    private final Locale _locale;
 
-    private String _fileName;
-    private Locale _locale = ENGLISH;
-
-    public AddTranslationTask(String taskName, String taskDescription, String fileName, Locale locale) {
+    /**
+     * Constructor for messages bundle registration.
+     *
+     * @param taskName task name
+     * @param taskDescription task description
+     * @param baseName bundle base name
+     * @param locale locale
+     */
+    public AddTranslationTask(String taskName, String taskDescription, String baseName, Locale locale) {
         super(taskName, taskDescription);
-        _fileName = fileName;
+        _baseName = baseName;
         _locale = locale;
     }
 
     @Override
     public void execute(InstallContext installContext) throws TaskExecutionException {
         ArrayDelegateTask task = new ArrayDelegateTask("Add translation key tasks");
-        ResourceBundle bundle = ResourceBundle.getBundle(_fileName, _locale);
+        ResourceBundle bundle = ResourceBundle.getBundle(_baseName, _locale);
         for (String key : Collections.list(bundle.getKeys())) {
             String keyNodeName = getValidatedLabel(key);
             task.addTask(
-                new NodeExistsDelegateTask("Check translation key", "Check translation key", WS_TRANSLATION, "/" + ND_TRANSLATION + "/" + keyNodeName, null,
+                new NodeExistsDelegateTask("Check translation key", "Check translation key", WS_TRANSLATION, "/" + keyNodeName, null,
                     new NodeBuilderTask("Create translation node", "", logging, WS_TRANSLATION,
-                        addOrGetNode(ND_TRANSLATION, NodeTypes.Folder.NAME).then(
-                            addOrGetNode(keyNodeName, Translation.NAME).then(
-                                addOrSetProperty(Translation.PN_KEY, key),
-                                addOrSetProperty(Translation.PREFIX_NAME + _locale.getLanguage(), bundle.getString(key))
-                            )
+                        addOrGetNode(keyNodeName, Translation.NAME).then(
+                            addOrSetProperty(Translation.PN_KEY, key),
+                            addOrSetProperty(Translation.PREFIX_NAME + _locale.getLanguage(), bundle.getString(key))
                         )
                     )
                 )
