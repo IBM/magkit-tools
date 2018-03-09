52f4889f17a5e8c56d99ef83e47ad22e112629fb
Wolf Bubenik <wolf.bubenik@aperto.de>
2018-03-09 16:12:09 +0100

    MGKT-468: fix support for basePath

--- src/main/java/com/aperto/magnolia/translation/AddTranslationTask.java
+++ src/main/java/com/aperto/magnolia/translation/AddTranslationTask.java
@@ -1,24 +1,26 @@
 package com.aperto.magnolia.translation;
 
 import com.aperto.magnolia.translation.TranslationNodeTypes.Translation;
+import info.magnolia.jcr.nodebuilder.NodeOperation;
 import info.magnolia.jcr.nodebuilder.task.NodeBuilderTask;
-import info.magnolia.jcr.util.NodeNameHelper;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.AbstractTask;
 import info.magnolia.module.delta.ArrayDelegateTask;
 import info.magnolia.module.delta.NodeExistsDelegateTask;
 import info.magnolia.module.delta.TaskExecutionException;
-import info.magnolia.objectfactory.Components;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import static org.apache.commons.lang3.StringUtils.EMPTY;
-import static org.apache.commons.lang3.StringUtils.defaultString;
 import static com.aperto.magnolia.translation.TranslationNodeTypes.WS_TRANSLATION;
+import static info.magnolia.cms.core.Path.getValidatedLabel;
 import static info.magnolia.jcr.nodebuilder.Ops.addNode;
 import static info.magnolia.jcr.nodebuilder.Ops.addProperty;
+import static info.magnolia.jcr.nodebuilder.Ops.getOrAddNode;
 import static info.magnolia.jcr.nodebuilder.task.ErrorHandling.logging;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.defaultString;
+import static org.apache.commons.lang3.StringUtils.removeStart;
 
 /**
  * Add all not existing keys from a properties file to translation workspace.
@@ -27,6 +29,7 @@ import static info.magnolia.jcr.nodebuilder.task.ErrorHandling.logging;
  */
 @SuppressWarnings("unused")
 public class AddTranslationTask extends AbstractTask {
+    private static final String ROOT_PATH = "/";
 
     private final String _baseName;
     private final Locale _locale;
@@ -52,7 +55,7 @@ public class AddTranslationTask extends AbstractTask {
      * @param taskDescription task description
      * @param baseName        bundle base name
      * @param locale          locale
-     * @param basePath        base path, may be null or empty or start with "/"
+     * @param basePath        base path
      */
     @SuppressWarnings("unused")
     public AddTranslationTask(String taskName, String taskDescription, String baseName, Locale locale, String basePath) {
@@ -66,21 +69,25 @@ public class AddTranslationTask extends AbstractTask {
     public void execute(InstallContext installContext) throws TaskExecutionException {
         ArrayDelegateTask task = new ArrayDelegateTask("Add translation key tasks");
         ResourceBundle bundle = ResourceBundle.getBundle(_baseName, _locale);
-        NodeNameHelper nodeNameHelper = Components.getComponent(NodeNameHelper.class);
 
         for (String key : bundle.keySet()) {
-            String keyNodeName = nodeNameHelper.getValidatedName(key);
+            String keyNodeName = getValidatedLabel(key);
             task.addTask(
                 new NodeExistsDelegateTask("Check translation key", "Check translation key", WS_TRANSLATION, _basePath + keyNodeName, null,
                     new NodeBuilderTask("Create translation node", "", logging, WS_TRANSLATION,
-                        addNode(_basePath + keyNodeName, Translation.NAME).then(
-                            addProperty(Translation.PN_KEY, (Object) key),
-                            addProperty(Translation.PREFIX_NAME + _locale.getLanguage(), (Object) bundle.getString(key))
-                        )
+                        getOp(keyNodeName, key, bundle.getString(key))
                     )
                 )
             );
         }
         task.execute(installContext);
+    }
+
+    private NodeOperation getOp(String keyNodeName, String key, String value) {
+        NodeOperation addKeyNode = addNode(keyNodeName, Translation.NAME).then(
+            addProperty(Translation.PN_KEY, key),
+            addProperty(Translation.PREFIX_NAME + _locale.getLanguage(), value)
+        );
+        return ROOT_PATH.equals(_basePath) ? addKeyNode : getOrAddNode(removeStart(_basePath, ROOT_PATH), Translation.NAME).then(addKeyNode);
     }
 }
