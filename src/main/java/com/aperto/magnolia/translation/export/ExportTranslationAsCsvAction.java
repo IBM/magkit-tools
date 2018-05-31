ae785932eb7fdf5e73d57ebabca36d84df847e59
frank sommer <frank.sommer@aperto.com>
2018-05-31 13:35:34 +0200

    MGKT-477: enable selective and multiple export
    (cherry picked from commit 1fa0722a3ad34947548fb1a2111d8ea7da9491ed)

--- src/main/java/com/aperto/magnolia/translation/export/ExportTranslationAsCsvAction.java
+++ src/main/java/com/aperto/magnolia/translation/export/ExportTranslationAsCsvAction.java
@@ -6,28 +6,36 @@ import com.vaadin.server.StreamResource;
 import info.magnolia.cms.core.Path;
 import info.magnolia.cms.i18n.I18nContentSupport;
 import info.magnolia.cms.util.QueryUtil;
+import info.magnolia.jcr.util.NodeUtil;
 import info.magnolia.ui.api.action.AbstractAction;
 import info.magnolia.ui.api.action.ActionExecutionException;
 import info.magnolia.ui.api.action.ConfiguredActionDefinition;
+import info.magnolia.ui.vaadin.integration.jcr.JcrItemAdapter;
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jcr.Node;
-import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
 
 import static com.aperto.magnolia.translation.TranslationNodeTypes.Translation.PREFIX_NAME;
 import static com.aperto.magnolia.translation.TranslationNodeTypes.WS_TRANSLATION;
+import static com.google.common.collect.Lists.newArrayList;
+import static info.magnolia.jcr.util.NodeUtil.asIterable;
+import static info.magnolia.jcr.util.NodeUtil.asList;
 import static info.magnolia.jcr.util.PropertyUtil.getString;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 /**
- * Create CSV file for all translations.
+ * Create CSV file for all translations or selected elements.
  *
  * @author diana.racho (Aperto AG)
  */
@@ -38,10 +46,16 @@ public class ExportTranslationAsCsvAction extends AbstractAction<ConfiguredActio
     private Map<String, Map<String, String>> _entries;
 
     private I18nContentSupport _i18nContentSupport;
+    private List<JcrItemAdapter> _items;
 
-    public ExportTranslationAsCsvAction(ConfiguredActionDefinition definition, I18nContentSupport i18nContentSupport) throws ActionExecutionException {
+    public ExportTranslationAsCsvAction(ConfiguredActionDefinition definition, I18nContentSupport i18nContentSupport, JcrItemAdapter item) throws ActionExecutionException {
+        this(definition, i18nContentSupport, newArrayList(item));
+    }
+
+    public ExportTranslationAsCsvAction(ConfiguredActionDefinition definition, I18nContentSupport i18nContentSupport, List<JcrItemAdapter> items) throws ActionExecutionException {
         super(definition);
         _i18nContentSupport = i18nContentSupport;
+        _items = items;
     }
 
     @Override
@@ -59,21 +73,39 @@ public class ExportTranslationAsCsvAction extends AbstractAction<ConfiguredActio
     private void setEntries(Collection<Locale> locales) {
         Map<String, Map<String, String>> entries = new TreeMap<>();
 
-        String statement = String.format(QUERY_STATEMENT, Translation.NAME);
-        try {
-            NodeIterator result = QueryUtil.search(WS_TRANSLATION, statement);
-            while (result.hasNext()) {
-                Node translationNode = result.nextNode();
-                Map<String, String> translationProperties = new TreeMap<>();
-                for (Locale locale : locales) {
-                    translationProperties.put(PREFIX_NAME + locale.getLanguage(), getString(translationNode, PREFIX_NAME + locale.getLanguage()));
-                }
-                entries.put(getString(translationNode, Translation.PN_KEY), translationProperties);
+        List<Node> t9nNodes = retrieveTranslationNodes();
+        for (Node t9nNode : t9nNodes) {
+            Map<String, String> translationProperties = new TreeMap<>();
+            for (Locale locale : locales) {
+                String propertyName = PREFIX_NAME + locale.getLanguage();
+                translationProperties.put(propertyName, getString(t9nNode, propertyName));
             }
-        } catch (RepositoryException e) {
-            LOGGER.error("Error querying all translations with query {}.", statement, e);
+            entries.put(getString(t9nNode, Translation.PN_KEY), translationProperties);
         }
+
         _entries = entries;
+    }
+
+    private List<Node> retrieveTranslationNodes() {
+        List<Node> t9nNodes = new ArrayList<>();
+        if (CollectionUtils.isEmpty(_items) || containsOnlyRootNode()) {
+            // query for all translation nodes
+            String statement = String.format(QUERY_STATEMENT, Translation.NAME);
+            try {
+                t9nNodes = asList(asIterable(QueryUtil.search(WS_TRANSLATION, statement)));
+            } catch (RepositoryException e) {
+                LOGGER.error("Error querying all translations with query {}.", statement, e);
+            }
+        } else {
+            for (JcrItemAdapter item : _items) {
+                t9nNodes.add((Node) item.getJcrItem());
+            }
+        }
+        return t9nNodes;
+    }
+
+    private boolean containsOnlyRootNode() {
+        return _items.size() == 1 && isEmpty(NodeUtil.getName((Node) _items.get(0).getJcrItem()));
     }
 
     private void streamFile(final TranslationCsvWriter csvWriter) {
