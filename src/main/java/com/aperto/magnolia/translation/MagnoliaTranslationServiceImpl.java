504a7f5209a2131b9c9c546abce10dcae1285875
Wolf Bubenik <wolf.bubenik@aperto.de>
2019-05-31 18:04:35 +0200

    MGKT-495: Allow distinction between missing and empty values; refactor to improve performance

--- src/main/java/com/aperto/magnolia/translation/MagnoliaTranslationServiceImpl.java
+++ src/main/java/com/aperto/magnolia/translation/MagnoliaTranslationServiceImpl.java
@@ -6,6 +6,7 @@ import info.magnolia.i18nsystem.DefaultMessageBundlesLoader;
 import info.magnolia.i18nsystem.LocaleProvider;
 import info.magnolia.i18nsystem.TranslationServiceImpl;
 import info.magnolia.i18nsystem.module.I18nModule;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,11 +16,12 @@ import javax.inject.Singleton;
 import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 
+import java.util.function.Predicate;
+
 import static com.aperto.magnolia.translation.TranslationNodeTypes.WS_TRANSLATION;
 import static info.magnolia.cms.util.QueryUtil.search;
 import static info.magnolia.jcr.util.PropertyUtil.getString;
 import static org.apache.commons.lang.StringUtils.EMPTY;
-import static org.apache.commons.lang.StringUtils.isNotBlank;
 
 /**
  * Translation service for templates.
@@ -30,7 +32,9 @@ import static org.apache.commons.lang.StringUtils.isNotBlank;
 @Singleton
 public class MagnoliaTranslationServiceImpl extends TranslationServiceImpl {
     private static final Logger LOGGER = LoggerFactory.getLogger(MagnoliaTranslationServiceImpl.class);
-    private static final String QUERY_STATEMENT = "select * from [%s] where key = '%s'";
+    private static final String BASE_QUERY = "select * from [" + Translation.NAME + "] where key = ";
+    // TODO: MGKT-495 - Change this condition to "NOT NULL" if empty Strings from App should be accepted
+    private static final Predicate<String> MESSAGE_CONDITION = StringUtils::isNotEmpty;
 
     @Inject
     public MagnoliaTranslationServiceImpl(Provider<I18nModule> i18nModuleProvider, Provider<DefaultMessageBundlesLoader> defaultMessageBundlesLoaderProvider) {
@@ -39,11 +43,19 @@ public class MagnoliaTranslationServiceImpl extends TranslationServiceImpl {
 
     @Override
     public String translate(LocaleProvider localeProvider, String basename, String[] keys) {
-        String message = super.translate(localeProvider, basename, keys);
+        // Performance: Query message properties only if needed - no acceptable message in App.
+        String message = getAppMessage(localeProvider, keys);
+        return MESSAGE_CONDITION.test(message) ? message : super.translate(localeProvider, basename, keys);
+    }
+
+    String getAppMessage(LocaleProvider localeProvider, String[] keys) {
         final String language = localeProvider.getLocale().getLanguage();
+        final String i18nProperty = Translation.PREFIX_NAME + language;
+        // get first acceptable translation for list of keys:
+        String message = null;
         for (String key : keys) {
-            String newMessage = getMessage(key, language);
-            if (isNotBlank(newMessage)) {
+            String newMessage = getMessage(key, i18nProperty);
+            if (MESSAGE_CONDITION.test(newMessage)) {
                 message = newMessage;
                 break;
             }
@@ -51,19 +63,17 @@ public class MagnoliaTranslationServiceImpl extends TranslationServiceImpl {
         return message;
     }
 
-    private String getMessage(String key, String language) {
-        String message = EMPTY;
+    private String getMessage(final String key, final String i18nProperty) {
+        // MGKT-495: Allow distinction between missing and empty values: return NULL for missing key
+        String message = null;
         if (!key.contains("'")) {
-            String statement = String.format(QUERY_STATEMENT, Translation.NAME, key);
+            // Replace string formatting - simple string concatenation is about 8 times faster.
+            String statement = BASE_QUERY + '\'' + key + '\'';
             try {
                 // MGKT-466: Execute translation search in system context > MgnlContext is not always set.
                 message = MgnlContext.doInSystemContext(() -> {
-                        String message1 = EMPTY;
                         NodeIterator nodeIterator = search(WS_TRANSLATION, statement);
-                        if (nodeIterator.hasNext()) {
-                            message1 = getString(nodeIterator.nextNode(), Translation.PREFIX_NAME + language, message1);
-                        }
-                        return message1;
+                        return nodeIterator.hasNext() ? getString(nodeIterator.nextNode(), i18nProperty, EMPTY) : null;
                     });
             } catch (RepositoryException e) {
                 LOGGER.error("Error on querying translation node for query {}.", statement, e);
