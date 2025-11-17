9cca40ae94b5e1f47abafae82534cc7f4a115db2
wolf.bubenik <wolf.bubenik@aperto.com>
2025-11-17 17:25:09 +0100

    MGKT-588: Complete tests

--- magkit-tools-app/src/test/java/de/ibmix/magkit/tools/app/QueryResultTableTest.java
+++ magkit-tools-app/src/test/java/de/ibmix/magkit/tools/app/QueryResultTableTest.java
@@ -20,8 +20,28 @@ package de.ibmix.magkit.tools.app;
  * #L%
  */
 
+import com.vaadin.server.Sizeable;
+import com.vaadin.v7.data.Item;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.query.QueryResult;
+import javax.jcr.query.Row;
+
+import java.util.Iterator;
+
+import static de.ibmix.magkit.test.jcr.RowStubbingOperation.stubPath;
+import static de.ibmix.magkit.test.jcr.RowStubbingOperation.stubValue;
+import static de.ibmix.magkit.test.jcr.query.QueryMockUtils.mockRow;
+import static de.ibmix.magkit.test.jcr.query.QueryMockUtils.mockRowQueryResult;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for QueryResultTable column name normalization.
@@ -30,12 +50,220 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @since 2017-12-05
  */
 public class QueryResultTableTest {
+
+    private QueryResultTable _resultTable;
+
+    @BeforeEach
+    void setUp() {
+        _resultTable = new QueryResultTable();
+    }
+
     @Test
-    public void normalizeColumnName() {
-        QueryResultTable resultTable = new QueryResultTable();
-        assertEquals("test", resultTable.normalizeColumnName("test"));
-        assertEquals("test.jcr:type", resultTable.normalizeColumnName("test.jcr:type"));
-        assertEquals("test", resultTable.normalizeColumnName("mgnl:page.test"));
-        assertEquals("jcr:type", resultTable.normalizeColumnName("mgnl:page.jcr:type"));
+    void testConstructor() {
+        QueryResultTable table = new QueryResultTable();
+
+        assertNotNull(table);
+        assertEquals(100f, table.getWidth());
+        assertEquals(Sizeable.Unit.PERCENTAGE, table.getWidthUnits());
+        assertEquals("searchresult", table.getStyleName());
+        assertTrue(table.isSortEnabled());
+    }
+
+    @Test
+    void testNormalizeColumnNameWithoutNamespace() {
+        assertEquals("test", _resultTable.normalizeColumnName("test"));
+    }
+
+    @Test
+    void testNormalizeColumnNameWithDotBeforeColon() {
+        assertEquals("test.jcr:type", _resultTable.normalizeColumnName("test.jcr:type"));
+    }
+
+    @Test
+    void testNormalizeColumnNameWithNamespaceBeforeDot() {
+        assertEquals("test", _resultTable.normalizeColumnName("mgnl:page.test"));
+    }
+
+    @Test
+    void testNormalizeColumnNameWithBothNamespaces() {
+        assertEquals("jcr:type", _resultTable.normalizeColumnName("mgnl:page.jcr:type"));
+    }
+
+    @Test
+    void testNormalizeColumnNameWithOnlyNamespace() {
+        assertEquals("jcr:type", _resultTable.normalizeColumnName("jcr:type"));
+    }
+
+    @Test
+    void testRetrievePrefixWithNamespace() {
+        assertEquals("", _resultTable.retrievePrefix("jcr:content"));
+    }
+
+    @Test
+    void testRetrievePrefixWithoutNamespace() {
+        assertEquals("page - ", _resultTable.retrievePrefix("page"));
+    }
+
+    @Test
+    void testRetrievePrefixWithEmptyString() {
+        assertEquals(" - ", _resultTable.retrievePrefix(""));
+    }
+
+    @Test
+    void testBuildResultTableWithNullQueryResult() throws RepositoryException {
+        _resultTable.buildResultTable(null, true, true, 100L);
+
+        assertEquals(0, _resultTable.size());
+        assertEquals(null, _resultTable.getCaption());
+    }
+
+    @Test
+    void testBuildResultTableWithShowScoreAndCols() throws RepositoryException {
+        QueryResult queryResult = mockRowQueryResult(
+            mockRow(0.5, stubPath("mgnl:page", "/test/path"), stubValue("jcr:title", "Test Title"), stubValue("jcr:uuid", "test-uuid"))
+
+        );
+
+        String[] selectorNames = new String[]{"mgnl:page"};
+        when(queryResult.getSelectorNames()).thenReturn(selectorNames);
+        String[] columnNames = new String[]{"jcr:title", "jcr:uuid"};
+        when(queryResult.getColumnNames()).thenReturn(columnNames);
+
+        _resultTable.buildResultTable(queryResult, true, true, 150L);
+
+        assertEquals(1, _resultTable.size());
+        assertEquals("Query result: 1 rows in 150ms", _resultTable.getCaption());
+        assertNotNull(_resultTable.getContainerProperty(_resultTable.getItemIds().iterator().next(), "path"));
+        assertNotNull(_resultTable.getContainerProperty(_resultTable.getItemIds().iterator().next(), "score"));
+        assertNotNull(_resultTable.getContainerProperty(_resultTable.getItemIds().iterator().next(), "jcr:title"));
+        assertNotNull(_resultTable.getContainerProperty(_resultTable.getItemIds().iterator().next(), "jcr:uuid"));
+    }
+
+    @Test
+    void testBuildResultTableWithMultipleRows() throws RepositoryException {
+        QueryResult queryResult = mockRowQueryResult(
+            mockRow(0.8, stubPath("mgnl:page", "/path1"), stubValue("jcr:title", "Test Title")),
+            mockRow(0.6, stubPath("mgnl:page", "/path2"), stubValue("jcr:title", "Test Title 2"))
+
+        );
+
+        String[] selectorNames = new String[]{"mgnl:page"};
+        when(queryResult.getSelectorNames()).thenReturn(selectorNames);
+        String[] columnNames = new String[]{"title"};
+        when(queryResult.getColumnNames()).thenReturn(columnNames);
+
+        _resultTable.buildResultTable(queryResult, true, false, 200L);
+
+        assertEquals(2, _resultTable.size());
+        assertEquals("Query result: 2 rows in 200ms", _resultTable.getCaption());
+    }
+
+    @Test
+    void testBuildResultTableWithoutShowScore() throws RepositoryException {
+        QueryResult queryResult = mockRowQueryResult(
+            mockRow(0.5, stubPath("mgnl:page", "/test/path"), stubValue("jcr:title", "Test Title"))
+        );
+
+        String[] selectorNames = new String[]{"mgnl:page"};
+        String[] columnNames = new String[]{"title"};
+
+        when(queryResult.getSelectorNames()).thenReturn(selectorNames);
+        when(queryResult.getColumnNames()).thenReturn(columnNames);
+
+        _resultTable.buildResultTable(queryResult, false, true, 100L);
+
+        assertEquals(1, _resultTable.size());
+        Object itemId = _resultTable.getItemIds().iterator().next();
+        assertEquals(null, _resultTable.getContainerProperty(itemId, "path"));
+        assertNotNull(_resultTable.getContainerProperty(itemId, "title"));
+    }
+
+    @Test
+    void testBuildResultTableWithoutShowCols() throws RepositoryException {
+        QueryResult queryResult = mockRowQueryResult(
+            mockRow(1.0, stubPath("mgnl:page", "/test"), stubValue("jcr:title", "Title"))
+        );
+
+        String[] selectorNames = new String[]{"mgnl:page"};
+        String[] columnNames = new String[]{"title"};
+
+        when(queryResult.getSelectorNames()).thenReturn(selectorNames);
+        when(queryResult.getColumnNames()).thenReturn(columnNames);
+
+        _resultTable.buildResultTable(queryResult, true, false, 50L);
+
+        assertEquals(1, _resultTable.size());
+        Object itemId = _resultTable.getItemIds().iterator().next();
+        assertNotNull(_resultTable.getContainerProperty(itemId, "path"));
+        assertNotNull(_resultTable.getContainerProperty(itemId, "score"));
+        assertEquals(null, _resultTable.getContainerProperty(itemId, "title"));
+    }
+
+    @Test
+    void testBuildResultTableWithMultipleSelectors() throws RepositoryException {
+        Row row1 = mockRow(0.9, stubPath("page", "/page/path"));
+        doReturn(0.9).when(row1).getScore("page");
+        Row row2 = mockRow(0.7, stubPath("asset", "/asset/path"));
+        doReturn(0.7).when(row2).getScore("asset");
+        QueryResult queryResult = mockRowQueryResult(
+            row1, row2
+        );
+
+        String[] selectorNames = new String[]{"page", "asset"};
+        when(queryResult.getSelectorNames()).thenReturn(selectorNames);
+        String[] columnNames = new String[]{};
+        when(queryResult.getColumnNames()).thenReturn(columnNames);
+
+        _resultTable.buildResultTable(queryResult, true, false, 300L);
+
+        assertEquals(2, _resultTable.size());
+        Iterator<?> itemIds = _resultTable.getItemIds().iterator();
+        Item item = _resultTable.getItem(itemIds.next());
+        String path = (String) item.getItemProperty("path").getValue();
+        String score = (String) item.getItemProperty("score").getValue();
+        // this is strange, but according to the implementation, if there is no value, "null" is appended
+        assertEquals("page - /page/path", path);
+        assertEquals("page - 0.9", score);
+
+        item = _resultTable.getItem(itemIds.next());
+        path = (String) item.getItemProperty("path").getValue();
+        score = (String) item.getItemProperty("score").getValue();
+        // this is strange, but according to the implementation, if there is no value, "null" is appended
+        assertEquals("asset - /asset/path", path);
+        assertEquals("asset - 0.7", score);
+    }
+
+    @Test
+    void testAddTableItemWithNullValue() throws RepositoryException {
+        String title = null;
+        String description = null;
+        QueryResult queryResult = mockRowQueryResult(
+            mockRow(1.0, stubPath("page", "/test"), stubValue("title", title), stubValue("description", description))
+        );
+
+        String[] selectorNames = new String[]{"page"};
+        when(queryResult.getSelectorNames()).thenReturn(selectorNames);
+        String[] columnNames = new String[]{"title", "description"};
+        when(queryResult.getColumnNames()).thenReturn(columnNames);
+
+        _resultTable.buildResultTable(queryResult, false, true, 100L);
+
+        assertEquals(1, _resultTable.size());
+        Object itemId = _resultTable.getItemIds().iterator().next();
+        assertNotNull(_resultTable.getContainerProperty(itemId, "title"));
+        assertNull(_resultTable.getContainerProperty(itemId, "title").getValue());
+        assertNotNull(_resultTable.getContainerProperty(itemId, "description"));
+        assertNull(_resultTable.getContainerProperty(itemId, "description").getValue());
+    }
+
+    @Test
+    void testBuildResultTableWithRepositoryException() throws RepositoryException {
+        QueryResult queryResult = mock(QueryResult.class);
+
+        when(queryResult.getColumnNames()).thenThrow(new RepositoryException("Test exception"));
+
+        _resultTable.buildResultTable(queryResult, true, true, 100L);
+
+        assertEquals(0, _resultTable.size());
     }
 }
